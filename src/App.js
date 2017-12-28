f0ae83ad711b16840fcd6cd0b1a74d9647fffd86
Peter de Croos <cultofmetatron@aumlogic.com>
2017-12-28 18:49:44 -0500

    changed root loader to laod App.js instead of menu directly

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 
 import logo from './logo.svg';
 import './App.css';
+import Menu from './components/Menu/Menu';
 
 class App extends Component {
   render () {
     let fa = "fa fa-heart";
     return (
       <div>
+        <h1>wut</h1>
         <Menu />
-        <h1> editable menu <h1>
+        <h1> editable menu </h1>
         <Menu editable={true} />
       </div>
     );
